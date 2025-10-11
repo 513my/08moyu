d8acf1a8aa2450dd83ecdd7eaf2248fb51e6207c
513my <aauacn@Outlook.com>
2025-10-11 20:09:29 +0800

    Add files via upload

--- js/132-1616-9977.js
+++ js/132-1616-9977.js
@@ -3,7 +3,7 @@
 <title>404 Not Found</title>
 </head><body>
 <h1>Not Found</h1>
-<p>The requested URL /res.daiyanbao.com/freevideojs/307/1/132-1616-9977.js was not found on this server.</p>
+<p>The requested URL /freevideojs/307/1/132-1616-9977.js was not found on this server.</p>
 <p>Additionally, a 404 Not Found
 error was encountered while trying to use an ErrorDocument to handle the request.</p>
 </body></html>
